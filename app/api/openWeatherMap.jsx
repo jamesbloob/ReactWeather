5a67e7f05efc2f5a7e9e1c5efcc0ebe70115029b
James Collins <james@asone.co.uk>
2017-06-29 18:07:12 +0100

    Finished App

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -3,18 +3,18 @@ var axios = require('axios');
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=2abba1f59955c63563b32084aef058cd&units=metric';
 
 module.exports = {
-  getTemp: function(location){
+  getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-    return axios.get(requestUrl).then(function(res){
-      if(res.data.cod && res.data.message) {
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
       } else {
         return res.data.main.temp;
       }
-    }, function(res) {
-      throw new Error(res.data.message);
+    }, function (err) {
+      throw new Error('Unable to fetch weather for that location.');
     });
   }
 }
